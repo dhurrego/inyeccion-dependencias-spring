b7acd522c9d5b15273665560e43266d73f6c2407
Deivid Urrego <deivid_199615@hotmail.com>
2021-02-07 23:22:06 -0500

    Inyeccion de dependencia con @AutoWired

---
+++ src/main/java/com/springboot/di/app/models/service/MiServicio.java
@@ -0,0 +1,12 @@
+package com.springboot.di.app.models.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MiServicio {
+	
+	public String operacion() {
+		return "Ejecutando una operación";
+	}
+	
+}
