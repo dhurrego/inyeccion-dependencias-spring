cb7cae4ac4cf48f9f84430bf003d3d546ec9d703
Deivid Urrego <deivid_199615@hotmail.com>
2021-02-16 17:40:49 -0500

    Usando la anotación @Qualifier para seleccionar que servicio se va a inyectar

--- src/main/java/com/springboot/di/app/controllers/IndexController.java
+++ src/main/java/com/springboot/di/app/controllers/IndexController.java
@@ -1,6 +1,7 @@
 package com.springboot.di.app.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +12,7 @@ import com.springboot.di.app.models.service.IMiServicio;
 public class IndexController {
 	
 	@Autowired
+	@Qualifier("miServicioSimple") // Si se elimina esta linea, Spring seleccionara el servicio que tenga la anotación @Primary
 	private IMiServicio servicio;
 
 	@GetMapping({"/", "", "/index"})
